44a92b08591d34fe13f07607b22694d96e469ebb
Abhinand sd <abhinandssd90@gmail.com>
2025-02-27 22:15:10 +0530

    second commit of frist epi create project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-const header = React.createElement("div", {}, [React.createElement("div", {id: "header2"}, [React.createElement("div", {id: "header2"}, React.createElement("h1", {id: "header2"}, "Frist heading"), React.createElement("div", {id: "header2"}, React.createElement("h1", {id: "header2"}, "second heading"))), React.createElement("div", {id: "header2"}, "ss")]), React.createElement("div", {id: "header2"}, React.createElement("h1", {id: "3rd"}, "thrid heading"))])
+const header = React.createElement("div", {}, [React.createElement("div", { id: "header2" }, [React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "header2" }, "Frist heading"), React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "header2" }, "second heading"))), React.createElement("div", { id: "header2" }, "ss")]), React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "3rd" }, "thrid heading"))])
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
