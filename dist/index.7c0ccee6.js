591879b0e1fc95fa3110dfca230be0f1d50a5aad
Abhinand sd <abhinandssd90@gmail.com>
2025-02-28 00:00:07 +0530

    react cdn removed

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,27 @@
+const header = React.createElement("div", {}, [
+    React.createElement("div", {
+        id: "header2"
+    }, [
+        React.createElement("div", {
+            id: "header2"
+        }, React.createElement("h1", {
+            id: "header2"
+        }, "Frist heading"), React.createElement("div", {
+            id: "header2"
+        }, React.createElement("h1", {
+            id: "header2"
+        }, "second heading"))),
+        React.createElement("div", {
+            id: "header2"
+        }, "ss")
+    ]),
+    React.createElement("div", {
+        id: "header2"
+    }, React.createElement("h1", {
+        id: "3rd"
+    }, "thrid heading"))
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
