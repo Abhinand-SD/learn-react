8dce161705dbec8d887bcfceb0e46c01e8e931ee
Abhinand sd <abhinandssd90@gmail.com>
2025-02-28 14:37:24 +0530

    create server with parcel

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 #leran react
+
+#Parcel
+
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm -written in c++
+-Caching - Faster  Builds
+-Image Optimization
+-Minification
+-Bulding
+-Compress
+-Consistent Hashing
+-Code Spliting
+-Differential Bundling - support older browsers
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree shaking -remove unused code
+-Different dev and prod bundles
+-
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
 
 const header = React.createElement("div", {}, [React.createElement("div", { id: "header2" }, [React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "header2" }, "Frist heading"), React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "header2" }, "second heading"))), React.createElement("div", { id: "header2" }, "ss")]), React.createElement("div", { id: "header2" }, React.createElement("h1", { id: "3rd" }, "thrid heading"))])
