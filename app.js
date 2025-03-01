ea78026a16cdb5d29c9d7d00f492c76bdb8dbf7e
Abhinand sd <abhinandssd90@gmail.com>
2025-03-01 23:59:03 +0530

    react components

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-//REACT -  REact.createElement-js Object => HTML element
+//REACT -  React.createElement-js Object => HTML element
 header = React.createElement("h1", {}, "This is react code")
-root = ReactDOM.createRoot(document.getElementById("root"))
+root = ReactDOM.createRoot(document.getElementById("root1"))
 root.render(header)
 
 
-// JSX -  jsx => React.CreateElement-js Object => HTML element 
+// JSX -  jsx =>Babel transpiles it to React.CreateElement-js Object => HTML element 
 root = ReactDOM.createRoot(document.getElementById("root2"))
 const jsxHeading = <h1> This is JSX code</h1>
 root.render(jsxHeading)
 
+
+//React Element
+const heading = (
+    <h1 className="head" tabIndex="5">
+        This is react element
+    </h1>
+)
+root = ReactDOM.createRoot(document.getElementById("root3"))
+root.render(heading)
+
+
+//function component
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading" > This is function combonent</h1>
+    </div>
+)
+root = ReactDOM.createRoot(document.getElementById("root4"))
+root.render(<HeadingComponent />)
+
+
+//function composition
+const Title = () => {
+    <h1>This is combonent combosition</h1>
+}
+
+
+const Headingomponent2 = () => (
+
+    <div id="container">
+        <Title />
+        <h1>This is function combosition</h1>
+    </div>
+)
+
+root = ReactDOM.createRoot(document.getElementById("root5"))
+root.render(<Headingomponent2 />)
